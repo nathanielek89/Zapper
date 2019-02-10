49d6629927cd47a395daf3ae48d56de43fe8435b
nathanielek89 <nathanielek89@gmail.com>
2019-02-09 22:09:06 -0600

    Added a button to pop up a message on the same view

--- app/src/main/java/com/Zapper/rating/MainActivity.java
+++ app/src/main/java/com/Zapper/rating/MainActivity.java
@@ -2,6 +2,9 @@ package com.Zapper.rating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
+import com.Zapper.rating.MainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SendMessage (View view){
+        //Toast.makeText(getActivity(), "Button Clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "No one is nearby", Toast.LENGTH_LONG).show();
     }
 }
